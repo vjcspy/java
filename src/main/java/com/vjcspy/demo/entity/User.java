707f3eae66d0f305251608d77f0452c8ae7d86ee
wsl <mr.vjcspy@gmail.com>
2024-10-20 12:26:02 +0700

    feat: init

---
+++ src/main/java/com/vjcspy/demo/entity/User.java
@@ -0,0 +1,46 @@
+package com.vjcspy.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
+    private String id;
+    private String username;
+    private String password;
+    private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
